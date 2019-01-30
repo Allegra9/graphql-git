6735462cb332ffad4f5da3678679b9b231d2ee81
Allegra <aukseallegra@gmail.com>
2019-01-30 17:48:45 +0000

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# GraphQL Git - Data Visualization in React
+# GraphQL GitHub - Data Visualization in React
 
-This is an app I made for an article which discusses how to use GraphQL in React utilizing the GitHub API. The first part of the article is about data manipulation to produce a pie chart, which can be found at: https://www.leighhalliday.com/map-reduce-filter-and-pie-charts
+Use GraphQL + React utilizing the GitHub API.
+Shows a pie chart of the most used languages of my GitHub repositories.
--- src/sampleData.js
+++ src/sampleData.js
@@ -1,89 +1,49 @@
 export default {
   data: {
-    viewer: {
-      repositories: {
-        nodes: [
-          {
-            name: "bloggy-blog",
-            description: "Code which powers my personal blog.",
-            url: "https://github.com/leighhalliday/bloggy-blog",
-            languages: {
-              nodes: [
-                {
-                  color: "#701516",
-                  name: "Ruby"
-                },
-                {
-                  color: "#f1e05a",
-                  name: "JavaScript"
-                },
-                {
-                  color: "#244776",
-                  name: "CoffeeScript"
-                },
-                {
-                  color: "#563d7c",
-                  name: "CSS"
-                },
-                {
-                  color: "#e34c26",
-                  name: "HTML"
-                }
-              ]
+    data: {
+      viewer: {
+        repositories: {
+          nodes: [
+            {
+              name: "gatsby-app",
+              description: null,
+              url: "https://github.com/Allegra9/gatsby-app",
+              languages: {
+                nodes: [
+                  {
+                    name: "JavaScript",
+                    color: "#f1e05a"
+                  },
+                  {
+                    name: "CSS",
+                    color: "#563d7c"
+                  }
+                ]
+              }
+            },
+            {
+              name: "hackernews-react-apollo",
+              description: null,
+              url: "https://github.com/Allegra9/hackernews-react-apollo",
+              languages: {
+                nodes: [
+                  {
+                    name: "HTML",
+                    color: "#e34c26"
+                  },
+                  {
+                    name: "JavaScript",
+                    color: "#f1e05a"
+                  },
+                  {
+                    name: "CSS",
+                    color: "#563d7c"
+                  }
+                ]
+              }
             }
-          },
-          {
-            name: "enumerables-presentation",
-            description: "A presentation about the Ruby Enumerable module",
-            url: "https://github.com/leighhalliday/enumerables-presentation",
-            languages: {
-              nodes: [
-                {
-                  color: "#f1e05a",
-                  name: "JavaScript"
-                },
-                {
-                  color: "#563d7c",
-                  name: "CSS"
-                },
-                {
-                  color: "#e34c26",
-                  name: "HTML"
-                },
-                {
-                  color: "#701516",
-                  name: "Ruby"
-                }
-              ]
-            }
-          },
-          {
-            name: "hotel",
-            description:
-              ":love_hotel: A simple process manager for developers. Start apps from your browser and access them using local domains",
-            url: "https://github.com/typicode/hotel",
-            languages: {
-              nodes: [
-                {
-                  color: "#f1e05a",
-                  name: "JavaScript"
-                },
-                {
-                  color: "#e34c26",
-                  name: "HTML"
-                },
-                {
-                  color: "#563d7c",
-                  name: "CSS"
-                },
-                {
-                  color: "#2b7489",
-                  name: "TypeScript"
-                }
-              ]
-            }
-          }
-        ]
+          ]
+        }
       }
     }
   }
